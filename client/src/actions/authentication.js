71239fdb04e087a5e7214f6300528bfadf85ca9c
Julian Diaz <diaz.julian@outlook.com>
2019-11-28 14:16:14 -0300

    fix with dispatch autehntication methods

--- client/src/actions/authentication.js
+++ client/src/actions/authentication.js
@@ -1,26 +1,45 @@
 import axios from 'axios';
-import { GET_ERRORS } from './actionTypes';
 
-export const registerUser = (user, history) => dispatch => {
-    axios.post('http://localhost:4001/users/', user)
-            .then(res => history.push('/login'))
-            .catch(err => {
-                dispatch({
-                    type: GET_ERRORS,
-                    payload: err.response.data
-                });
-            });
+// export const registerUser = (user, history) => dispatch => {
+//     axios.post('http://localhost:4001/users/', user)
+//             .then(() => history.push('/login'))
+//             .catch(err => {
+//                 dispatch({
+//                     type: GET_ERRORS,
+//                     payload: err.response
+//                 });
+//             });
+// };
+
+export function registerUser(user, history) {
+  return () => (
+    new Promise((resolve, reject) => {
+      axios.post('http://localhost:4001/users', user)
+        .then(() => history.push('/login'))
+        .catch((err) => {
+          console.log(err);
+          const error = err.response.data;
+          return reject(error);
+        });
+    })
+  );
 }
 
-export const loginUser = (user) => dispatch => {
-    axios.post('http://localhost:4001/users/login', user)
-            .then(res => {
-                console.log(res.data);
-            })
-            .catch(err => {
-                dispatch({
-                    type: GET_ERRORS,
-                    payload: err.response.data
-                });
-            });
+export function loginUser(user) {
+  return dispatch => (
+    new Promise((resolve, reject) => {
+      axios.post('http://localhost:4001/users/login', user)
+        .then((res) => {
+          console.log(res.data);
+
+          dispatch(action.authLogin(res.data));
+          return resolve();
+        })
+        .catch((err) => {
+          console.log(err);
+          const error = err.response.data;
+          return reject(error);
+        });
+    })
+  );
 }
