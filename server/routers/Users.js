dab0153b140c3c8c4e034d3f6f463b7de44927a2
Julian Diaz <diaz.julian@outlook.com>
2019-11-28 14:24:34 -0300

    ultramegasuper fix of bad references to User instead of Usario

--- server/routers/Users.js
+++ server/routers/Users.js
@@ -8,12 +8,12 @@ Router.post('/users', async (req, res) => {
     // CREA UN NUEVO User
     try {
       console.log(req.body)
-        const User = new Usuario(req.body)
-        await User.save()
-        const token = await User.generateAuthToken()
-        res.status(200).send({ codigo: 200, mensaje:'Registro OK', User, token })
+        const Usuario = new Usuario(req.body)
+        await Usuario.save()
+        const token = await Usuario.generateAuthToken()
+        res.status(200).send({ codigo: 200, mensaje:'Registro OK', Usuario, token })
     } catch (error) {
-      console.log(error)
+      console.log("register error: " + error)
         if(error.code == 11000)
         res.status(400).send({ codigo: 400, mensaje: 'Email ya usado', error })
         res.status(400).send({ codigo: 400, mensaje: 'Error al registrar', error })
@@ -24,29 +24,30 @@ Router.post('/users/login', async(req, res) => {
     // ACCESO DE UserS REGISTRADOS
     try {
         const { email, password } = req.body
-        const User = await User.findByCredentials(email, password)
-        if (!User) {
+        const Usuario = await Usuario.findByCredentials(email, password)
+        if (!Usuario) {
             return res.status(400).send({codigo: 400, error: 'Error! Verifique sus credenciales'})
         }
-        const token = await User.generateAuthToken()
-        res.send({ codigo: 200, mensaje: 'Inicio de sesión exitoso', User, token })
+        const token = await Usuario.generateAuthToken()
+        res.send({ codigo: 200, mensaje: 'Inicio de sesión exitoso', Usuario, token })
     } catch (error) {
+      console.log("login error: " + error)
         res.status(400).send({ codigo: 400, mensaje: 'Error durante el login' })
     }
 })
 
 Router.get('/users/account', Auth, async(req, res) => {
     // VER EL User CON SESION ABIERTA
-    res.send(req.User)
+    res.send(req.Usuario)
 })
 
 Router.post('/users/account/logout', Auth, async (req, res) => {
     // CERRAR SESION DEL USUARIO
     try {
-        req.User.tokens = req.User.tokens.filter((token) => {
+        req.Usuario.tokens = req.Usuario.tokens.filter((token) => {
             return token.token != req.token
         })
-        await req.User.save()
+        await req.Usuario.save()
         res.status(200).send({codigo: 200, mensaje: 'Sesion cerrada'})
     } catch (error) {
         res.status(400).send({codigo: 400, mensaje: 'Error al cerrar sesión', error})
