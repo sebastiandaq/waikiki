05f7cfdfd70dc815cfc135c964cd28cbf560f495
Julian Diaz <diaz.julian@outlook.com>
2019-11-29 14:22:15 -0300

    adds css for order items

--- client/src/components/cart/OrderDetails.js
+++ client/src/components/cart/OrderDetails.js
@@ -10,26 +10,19 @@ const OrderDetails = ({order}) => {
           </div>
         </div>
 
-        {order.items && order.items.length > 0 && order.items.map((item, index) => {
-            return (
-              <div className="col-lg-7 col-md-7 col-sm-6 col-xs-12">
-              <div className="cart-text-content-section">
-                <div className="cart-item-title">{item.title}</div>
-                <div className="cart-item-description">{item.description}</div>
-                <div className="cart-item-numbers">Cantidad: {item.count}</div>
-              </div>
-            </div>
-            );
-          })}
 
-        {/* <div className="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-          <div className="cart-cumulative-price-section">
-            <div className="cart_item_delete-container" data-id={cart_item.id} onClick={onDelete} title="Delete from cart">
-              <span className="glyphicon glyphicon-trash"></span>
-            </div>
-            <div className="cart-cumulative-price">ARS. {cart_item.count * cart_item.price}</div>
+        <div className="col-lg-7 col-md-7">
+          <div className="cart-text-content-section">
+            {order.items && order.items.length > 0 && order.items.map((item, index) => {
+              return (
+                <div className="order-item">
+                  <div className="order-item-title">{item.title}</div>
+                  <div className="order-item-description">{item.description} - Cantidad: {item.count} - Precio Unitario: {item.price}</div>
+                </div>
+              );
+            })}
           </div>
-        </div> */}
+        </div>
 
       </div>
     </div>
